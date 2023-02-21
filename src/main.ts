a05c2b5c01835aa68d027a896217dd8c4d76ed9d
ppxb <317842449@qq.com>
2023-02-21 16:30:18 +0800

    chore: add store

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
 import { elementPlus } from '@/plugins/element-plus'
+import router from './router'
+import store from './store'
+import App from './App.vue'
+
 import 'uno.css'
 import './index.less'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 app.use(elementPlus)
 
 app.mount('#app')
